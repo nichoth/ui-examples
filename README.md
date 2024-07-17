db8eb187fd710c01ad9e9ddb6df13c33e65823a0
Nick Thomas <nichoth@nichoth.com>
2024-07-16 17:18:38 -0700

    add another example

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 ## example 1
 
 A link with an animated bottom border on hover, originally [seen here](https://www.filamentgroup.com/lab/).
+
+## example 2
+
+A link with a wavy border hover state, [as seen here](https://sarajoy.dev/)
--- example/index.ts
+++ example/index.ts
@@ -1,5 +1,13 @@
 import './index.css'
 
+// example 1
 document.body.innerHTML += `
     <a class="button" href="#">example</a>
+`
+
+// example 2
+document.body.innerHTML += `
+<div>
+    <a href="#" class="wavy">example link two</a>
+</div>
 `
